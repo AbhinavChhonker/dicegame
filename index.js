7132e5df98a23fb6b2425920673596ac7442b2d8
AbhinavChhonker <53707961+AbhinavChhonker@users.noreply.github.com>
2019-09-23 17:56:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function playMe() {
   if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩Player1 wins";
   } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player2 wins🚩";
+    document.querySelector("h1").innerHTML = "Player2 wins 🚩";
   } else {
     document.querySelector("h1").innerHTML = "Draw!";
   }
